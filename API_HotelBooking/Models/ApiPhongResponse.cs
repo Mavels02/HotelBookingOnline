b579eae38351e1a643741fd38fdf4cbddfcf959e
Mavels02 <nghitran91002@gmail.com>
2025-05-16 16:40:39 +0700

    push source

---
+++ API_HotelBooking/Models/ApiPhongResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace API_HotelBooking.Models
+{
+    public class ApiPhongResponse
+
+    {
+        public IEnumerable<Phong> Rooms { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
